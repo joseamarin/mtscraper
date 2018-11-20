4ed023946933405475f32eb7c9b53e771d934570
Jose Marin <josemarinemail@gmail.com>
2018-11-20 15:10:40 -0500

    updated lilypad and left unsub as placeholder

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 (function(window, document) {
 	'use strict';
 
-	const template = "ADV (9) Green and (1397) Lillypad 4 Packages:\n\nHi Team,\n\nPlease find your offer ADV here listed below and attached:\n\nOffer(s):\n(ID) ADV\n\nAffiliate(s):\nGreen (9)\nLillypad4 (1397)\n\nCreative(s):\nCreative Name here\n\nSubject Line(s):\nSL\n\nFrom Line(s):\nFL\n\nUnsubscribe URL:\nUNSUB\n\nPreapproval seeds:\n\nzxtest@zetainteractive.com\n\nThanks.";
+	const template = "ADV (9) Green and (1397) PlutoC Packages:\n\nHi Team,\n\nPlease find your offer ADV here listed below and attached:\n\nOffer(s):\n(ID) ADV\n\nAffiliate(s):\nGreen (9)\nPlutoC (1397)\n\nCreative(s):\nCreative Name here\n\nSubject Line(s):\nSL\n\nFrom Line(s):\nFL\n\nUnsubscribe URL:\nUNSUB\n\nPreapproval seeds:\n\nzxtest@zetainteractive.com\n\nThanks.";
 
 	const advName = document.getElementsByTagName('input')[3].attributes.value.nodeValue;
 
@@ -45,7 +45,7 @@
 		template = template.replace(/ID/g, cakeOfferId);
 		template = template.replace(/SL/g, subs.join('\n'));
 		template = template.replace(/FL/g, froms.join('\n'));
-		template = template.replace(/UNSUB/g, unsub);
+		// template = template.replace(/UNSUB/g, unsub);
 		return template;
 	}
 
