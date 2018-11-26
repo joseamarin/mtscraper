631f3f0fbe236e018b400d7d06637f72a64cdcb9
Jose Marin <josemarinemail@gmail.com>
2018-11-26 12:57:11 -0500

    refactor of code along with bug fixes and style updates

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # mtscraper
 # [Find Here](https://joseamarin.github.io/mtscraper)
-# Getting Started
--- js/main.js
+++ js/main.js
@@ -1,63 +1,181 @@
 (function(window, document) {
 	'use strict';
 
-	const template = "ADV (9) Green and (1397) PlutoC Packages:\n\nHi Team,\n\nPlease find your offer ADV here listed below and attached:\n\nOffer(s):\n(ID) ADV\n\nAffiliate(s):\nGreen (9)\nPlutoC (1397)\n\nCreative(s):\nCreative Name here\n\nSubject Line(s):\nSL\n\nFrom Line(s):\nFL\n\nUnsubscribe URL:\nUNSUB\n\nPreapproval seeds:\n\nzxtest@zetainteractive.com\n\nThanks.";
+	function OfferLinkDirector ( strategyFactory ) {
+		this.factory = strategyFactory;
 
-	const advName = document.getElementsByTagName('input')[3].attributes.value.nodeValue;
+		if ( !this.factory ) {
+			this.factory = new OfferIdParserFactory();
+		}
 
-	const subFromReplace = sl => {
-		sl = sl.replace(/\(Active - A - AA \)/g, '');
-		sl = sl.replace(/^[0-9]{0,9999}/g, '');
-		sl = sl.replace(/-/, '');
-		return sl;
+		this.getOfferId = function ( rawContext ) {
+			const strategyList = [ 'Cid' , 'Oid' , 'Oxid' ];
+			let currentStrategyName;
+			while ( currentStrategyName = strategyList.pop() ) {
+				let strategy = this.factory[ 'get' + currentStrategyName + 'Strategy' ]();
+
+				if ( strategy.isValidContext( rawContext ) ) {
+					return strategy.extractOfferId( rawContext );
+				}
+			}
+		};
 	}
 
-	if (document.getElementsByName('csubject')[0].length === 1) {
-		const sl = document.getElementsByName('csubject')[0].innerText;
-		subFromReplace(sl);
+	function OfferIdStrategy ( validityHandler , extractionHandler ) {
+		this.config = {
+			"validate" : validityHandler ,
+			"extract" : extractionHandler
+		};
+
+		this.isValidContext = function ( rawContext ) {
+			return this.config.validate( rawContext );
+		};
+
+		this.extractOfferId = function ( rawContext ) {
+			return this.config.extract( rawContext );
+		}
 	}
 
-	const sl = document.getElementsByName('csubject')[0];
-	const subs = [];
-	Array.from(sl).forEach(e => {
-		subs.push(subFromReplace(e.innerText));
-	});
+	const OfferIdParserFactory = function () {
+		const self = this;
 
-	if (document.getElementsByName('from')[0].length === 1) {
-		const fl = document.getElementsByName('from')[0].innerText;
-		subFromReplace(fl);
+		self.getDirector = function () {
+			return new OfferLinkDirector( self );
+		};
+
+		self.getCidStrategy = function () {
+			return new OfferIdStrategy(
+				function ( rawContext ) {
+					const context = rawContext.substring( rawContext.indexOf( 'Cake Offer ID::' ) ).substring( 28 );
+					const isNotNull = ( context.match( /^[0-9]/g ) !== null );
+					const regexMatches = context.match( /^([0-9])(\d+)/g );
+					if ( isNotNull && context.match( /^[0-9]/g )[ 0 ] ) {
+						return isNotNull && context.match( /^[0-9]/g )[ 0 ][ 'length' ] === 1 &&
+							Number( context.match( /^([0-9])/g )[ 0 ] > 0 );
+					}
+					else {
+						return isNotNull && regexMatches[ 0 ] !== null && Number( regexMatches[ 0 ] ) > 0;
+					}
+
+				} ,
+				function ( rawContext ) {
+					if (rawContext
+						.substring( rawContext.indexOf( 'Cake Offer ID::' ) )
+						.substring( 28 ).match( /^[0-9](\d+)|^[0-9]/g )[ 0 ][ 'length' ] === 1)
+					{
+						return rawContext
+							.substring( rawContext.indexOf( 'Cake Offer ID::' ) )
+							.substring( 28 )
+							.match( /^([0-9])/g )[ 0 ];
+					}
+					return rawContext
+						.substring( rawContext.indexOf( 'Cake Offer ID::' ) )
+						.substring( 28 )
+						.match( /^([0-9])(\d+)/g )[ 0 ];
+				}
+			);
+		};
+
+		self.getOidStrategy = function () {
+			return new OfferIdStrategy(
+				function ( rawContext ) {
+					const context = rawContext.substring( rawContext.indexOf( '?o=' ) );
+					const regexMatches = context.match( /\?o=(\d+)([a-zA-Z$!#]+)?&.+/ );
+
+					return regexMatches !== null && Number( regexMatches[ 1 ] ) > 0;
+				} ,
+				function ( rawContext ) {
+					return rawContext
+						.substring( rawContext.indexOf( '?o=' ) )
+						.match( /\?o=(\d+)([a-zA-Z$!#]+)?&.+/ )[ 1 ];
+				}
+			);
+		};
+
+		self.getOxidStrategy = function () {
+			return new OfferIdStrategy(
+				function ( rawContext ) {
+					const context = rawContext.substring( rawContext.indexOf( 'oxid=' ) );
+					const regexMatches = context.match( /\oxid=(\d+)([a-zA-Z$!#]+)?&.+/ );
+
+					return regexMatches !== null && regexMatches[ 1 ] > 0;
+				} , 
+				function ( rawContext ) {
+					return rawContext
+						.substring( rawContext.indexOf( '?oxid=' ) )
+						.match( /\oxid=(\d+)([a-zA-Z$!#]+)?&.+/ )[ 1 ];
+				}
+			);
+		};
+	};
+
+
+	/**
+	 *  *  * Main Script
+	 *   *   */
+	const director = new OfferLinkDirector();
+
+	let cakeOfferId;
+	if (cakeOfferId = director.getOfferId(document.getElementsByTagName('table')[10].innerHTML)) {
+		cakeOfferId = director.getOfferId(
+			document.getElementsByTagName('table')[10].innerHTML
+		);
+	}
+	else {
+		cakeOfferId = 'Cake Offer ID Not found';
 	}
 
-	const fl = document.getElementsByName('from')[0];
-	const froms = [];
-	Array.from(fl).forEach(e => {
-		froms.push(subFromReplace(e.innerText));
-	});
+	const advertiserName = document.getElementsByTagName('input')[3].attributes.value.nodeValue;
 
-	const tenthTable = document.getElementsByTagName('table')[10].innerHTML;
-	const cloc = tenthTable.indexOf('Cake Offer ID::');
-	const cakeOfferId = tenthTable.substring(cloc, cloc+33).substring(cloc, 28);
+	const subFromFilter = (unfilteredContext) => {
+		return unfilteredContext
+			.replace(/\(Active - A - AA \)/g, '')
+			.replace(/\(Active - O - AA \)/g, '')
+			.replace(/^[0-9]{0,}/g, '')
+			.replace(/^[0-9]{0,}/g, '')
+			.replace(/-/, '')
+			.replace(/\(Inactive - A - NA! \)/g, '')
+			.replace(/^.*\(Inactive - A - AA \)/g, '');
+	}
+
+	const createArray = (domElement) => {
+		const arr = [];
+		Array.from(domElement).forEach((s) => {
+			arr.push(subFromFilter(s.innerText));
+		});
+		return arr.join('\n').split('\n')
+			.map(function(v) {
+				return v.trim()
+			}).filter(function(v) {
+				return v != '';
+			}).join('\n');
+	}
+
+	const subs = createArray(document.getElementsByName('csubject')[0]);
+	const froms = createArray(document.getElementsByName('from')[0]);
 
 	const unsub = document.getElementsByName('unsub_link')[0].attributes.value.nodeValue;
 
-	const buildEmail = template => {
-		template = template.replace(/ADV/g, advName);
-		template = template.replace(/ID/g, cakeOfferId);
-		template = template.replace(/SL/g, subs.join('\n'));
-		template = template.replace(/FL/g, froms.join('\n'));
-		// template = template.replace(/UNSUB/g, unsub);
-		return template;
+	const pkgTemplate = "ADV (9) Green and (1397) PlutoC Packages:\n\nHi Team,\n\nPlease find your offer ADV here listed below and attached:\n\nOffer(s):\n(ID) ADV\n\nAffiliate(s):\nGreen (9)\nPlutoC (1397)\n\nCreative(s):\nCreative Name here\n\nSubject Line(s):\nSL\n\nFrom Line(s):\nFL\n\nUnsubscribe URL:\nUNSUB\n\nPreapproval seeds:\nzxtest@zetainteractive.com\n\nThanks.";
+
+	const buildEmail = pkgTemplate => {
+		pkgTemplate = pkgTemplate.replace(/ADV/g, advertiserName);
+		pkgTemplate = pkgTemplate.replace(/ID/g, cakeOfferId);
+		pkgTemplate = pkgTemplate.replace(/SL/g, subs);
+		pkgTemplate = pkgTemplate.replace(/FL/g, froms);
+		// pkgTemplate = pkgTemplate.replace(/UNSUB/g, unsub);
+		return pkgTemplate;
 	}
 
 	const textarea = document.createElement('textarea');
 	textarea.classList.add('js-tmp');
-	textarea.innerHTML = buildEmail(template);
+	textarea.innerHTML = buildEmail(pkgTemplate);
 	const textToCopy = document.querySelector('.js-tmp');
 	document.querySelector('table').appendChild(textarea);
 	textarea.focus();
 	textarea.select();
 
-	const tryToCopy = () => {
+	const copyEmailTemplate = () => {
 		try {
 			document.execCommand('copy');
 		}
@@ -69,7 +187,7 @@
 		}
 	}
 
-	const ffCopy = () => {
+	const ffCopyEmailTemplate = () => {
 		document.body.appendChild(textarea);
 		textarea.focus();
 		textarea.select();
@@ -84,25 +202,45 @@
 		}
 	}
 
-	const showCopyPage = () => {
+	const renderEmailTemplate = () => {
 		const div = document.createElement('div');
-		const mainTable = document.getElementsByTagName('table')[0];
+		const header = document.createElement('div');
+		const headerText = document.createElement('p');
+		const root = document.getElementsByTagName('table')[0];
 		const copyBtn =  document.createElement('button');
-		copyBtn.classList.add('cpy-btn');
-		copyBtn.innerText = 'COPY';
-		mainTable.style.display = 'none';
+		copyBtn.style.backgroundImage = 'url()';
+		copyBtn.style.position = 'fixed';
+		copyBtn.style.top = '50%';
+		copyBtn.style.right = '50%';
+		copyBtn.style.height = '25px';
+		copyBtn.style.width = '55px';
+		copyBtn.style.backgroundRepeat = 'no-repeat';
+		copyBtn.style.backgroundSize = 'cover';
+		root.style.display = 'none';
 		div.innerText = textarea.textContent;
+		header.style.backgroundColor = '#509C10';
+		headerText.innerText = 'Email Package Template'
+		headerText.style.color = '#fff';
+		headerText.style.fontFamily = 'Verdana,Arial,Helvetica,sans-serif';
+		headerText.style.fontSize = '13px';
+		headerText.style.textAlign = 'center';
+		headerText.style.fontWeight = 'bold';
+		headerText.style.margin = 0;
+		div.style.backgroundColor = '#E3FAD1';
+		header.appendChild(headerText);
+		document.body.appendChild(header);
 		document.body.appendChild(div);
-		document.body.appendChild(copyBtn);
+		div.appendChild(copyBtn);
 		copyBtn.addEventListener('click', event => {
-			ffCopy();
-			mainTable.style.display = 'block';
+			ffCopyEmailTemplate();
+			root.style.display = 'block';
 			document.body.removeChild(div);
+			document.body.removeChild(header);
 			textarea.style.display = 'none';
 			copyBtn.style.display = 'none';
 		});
 	}
 
 	const browser = navigator.userAgent.toLowerCase();
-	browser.indexOf('firefox') > -1 ? showCopyPage() : tryToCopy();
+	browser.indexOf('firefox') > -1 ? renderEmailTemplate() : copyEmailTemplate();
 })(this, document);
